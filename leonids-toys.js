3ab58430d05cb7c37b57607bd43042e8b2cfbd4c
allisonkosborne <allisonkosborne@me.com>
2021-10-30 19:41:46 -0500

    first round of toy descriptions

---
+++ leonids-toys.js
@@ -0,0 +1,35 @@
+const robotTrex = {
+    cost: 200,
+    material: plastic,
+    height: 24,
+    width: 16,
+    weight: 16,
+    color: black, 
+    ageAppropriate: 12,
+    type: robot, 
+
+}
+
+const easyBakeOven = {
+    cost: 75,
+    materials: aluminum,
+    height: 8,
+    width: 14,
+    weight: 120,
+    color: purple, 
+    ageAppropriate: 8, 
+    type: kitchen, 
+
+}
+
+const sillyPutty = {
+    cost: 2,
+    materials: plastic, 
+    height: 1,
+    width: 2,
+    weight: 4,
+    color: yellow, 
+    ageAppropriate: 6, 
+    type: classic, 
+
+}
